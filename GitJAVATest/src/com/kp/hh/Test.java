ee2e4a0be78869550eff6f8e90072d0ffd182e54
alex110 <1061354225@qq.com>
2017-02-17 16:09:02 +0800

    第一次提交

---
+++ GitJAVATest/src/com/kp/hh/Test.java
@@ -0,0 +1,20 @@
+package com.kp.hh;
+/** 
+ * @author: py
+ * @version:2017年2月17日 下午3:34:50 
+ * com.kp.hh.Test.java
+ * @Desc 
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.err.println("hhahaha ");
+		System.err.println("hhahaha ");
+		System.err.println("hhahaha ");
+		System.err.println("hhahaha ");
+		System.err.println("hhahaha ");
+		System.err.println("hhahaha ");
+//		/GitJAVATest
+	}
+
+}
